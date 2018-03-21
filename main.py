fd794cc46f1d24764c1cba9860a2107b1482ae7a
Swetanjal Dutta <swetanjal.dutta@research.iiit.ac.in>
2018-03-21 10:01:54 +0530

    Basic framework of the game. The spaceship can move and aliens are randomly spawned and destructed according to the given rules. Type 1 missiles are generated properly and they can move properly

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ red = (255,0,0)
 ###################################################
 
 #Implement inheritance of character
-	
+#Care must be taken that two aliens are not spawned at the same place
 ###################################################
 class Spaceship():
 	def __init__(self, gameDisplay, x, y, width=100, height=75):
@@ -34,7 +34,19 @@ class Alien():
 		pygame.draw.rect(self.display, self.color, [self.x , self.y , self.width , self.height])
 
 ###################################################
+class Missile1:
+	def __init__(self, gameDisplay, x, y, width=100, height=75):
+		self.height=height
+		self.width=width
+		self.x=x
+		self.y=y
+		self.display=gameDisplay
+		self.color = black
+		self.speed = -2.5
+	def draw(self):
+		pygame.draw.rect(self.display, self.color, [self.x , self.y , self.width , self.height])
 
+###################################################
 pygame.init()
 gameDisplay = pygame.display.set_mode((800,600))
 clock = pygame.time.Clock()
@@ -48,6 +60,7 @@ space_ship = Spaceship(gameDisplay, 0, 525)
 timer = 0
 aliens = []
 aliens.append(Alien(gameDisplay, random.choice([0,100,200,300,400,500,600,700]), random.choice([1,0])*75, timer+(30*8)))
+missiles = []
 change = 0
 
 while not gameExit:
@@ -63,11 +76,16 @@ while not gameExit:
 		if event.type == pygame.KEYUP:
 			if event.key == pygame.K_d or event.key == pygame.K_a:
 				change=0
+			if event.key == pygame.K_SPACE:
+				missiles.append(Missile1(gameDisplay, space_ship.x, space_ship.y-75))
 	if timer%300 == 0:
 		aliens.append(Alien(gameDisplay, random.choice([0,100,200,300,400,500,600,700]), random.choice([1,0])*75, timer+(30*8)))
-	for i in range(len(aliens)):
+	i=0
+	while i < (len(aliens)):
 		if timer == aliens[i].death_time:
 			aliens.pop(i)
+			i=i-1
+		i=i+1
 	if change<0 and space_ship.x>-10:
 		space_ship.x+=change
 	if change>0 and space_ship.x<710:
@@ -76,6 +94,15 @@ while not gameExit:
 	space_ship.draw()
 	for alien in aliens:
 		alien.draw()
+	for missile in missiles:
+		missile.y+=missile.speed
+		missile.draw()
+	i=0
+	while i < (len(missiles)):
+		if missiles[i].y<-100:
+			missiles.pop(i)
+			i=i-1
+		i=i+1
 	pygame.display.update()
 	clock.tick(30)
 pygame.quit()
