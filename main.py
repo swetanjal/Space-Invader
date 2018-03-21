e3fbc3a3131a09a18a916b2d17bb5fee43551950
Swetanjal Dutta <swetanjal.dutta@research.iiit.ac.in>
2018-03-21 14:16:56 +0530

    Basic structure of the game ready without scoreboard

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import random
 black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
+blue = (0,0,255)
+yellow = (255,255,0)
 ###################################################
 
 #Implement inheritance of character
@@ -46,6 +48,18 @@ class Missile1:
 	def draw(self):
 		pygame.draw.rect(self.display, self.color, [self.x , self.y , self.width , self.height])
 
+class Missile2:
+	def __init__(self, gameDisplay, x, y, width=100, height=75):
+		self.height=height
+		self.width=width
+		self.x=x
+		self.y=y
+		self.display=gameDisplay
+		self.color = blue
+		self.speed = -5.0
+	def draw(self):
+		pygame.draw.rect(self.display, self.color, [self.x , self.y , self.width , self.height])	
+
 ###################################################
 pygame.init()
 gameDisplay = pygame.display.set_mode((800,600))
@@ -60,9 +74,10 @@ space_ship = Spaceship(gameDisplay, 0, 525)
 timer = 0
 aliens = []
 aliens.append(Alien(gameDisplay, random.choice([0,100,200,300,400,500,600,700]), random.choice([1,0])*75, timer+(30*8)))
-missiles = []
+missiles1 = []
+missiles2 = []
 change = 0
-
+score=0
 while not gameExit:
 	timer = timer+1
 	for event in pygame.event.get():
@@ -77,12 +92,16 @@ while not gameExit:
 			if event.key == pygame.K_d or event.key == pygame.K_a:
 				change=0
 			if event.key == pygame.K_SPACE:
-				missiles.append(Missile1(gameDisplay, space_ship.x, space_ship.y-75))
+				missiles1.append(Missile1(gameDisplay, space_ship.x, space_ship.y-75))
+			if event.key == pygame.K_s:
+				missiles2.append(Missile2(gameDisplay, space_ship.x, space_ship.y-75))
 	if timer%300 == 0:
 		aliens.append(Alien(gameDisplay, random.choice([0,100,200,300,400,500,600,700]), random.choice([1,0])*75, timer+(30*8)))
 	i=0
 	while i < (len(aliens)):
 		if timer == aliens[i].death_time:
+			if aliens[i].color == yellow:
+				score+=1
 			aliens.pop(i)
 			i=i-1
 		i=i+1
@@ -92,15 +111,43 @@ while not gameExit:
 		space_ship.x+=change
 	gameDisplay.fill(white)
 	space_ship.draw()
+	i=0
+	while i<len(missiles1):
+		j=0
+		while j<len(aliens):
+			if aliens[j].x<missiles1[i].x+missiles1[i].width and missiles1[i].x<aliens[j].x+aliens[j].width and aliens[j].y < missiles1[i].y and missiles1[i].y<aliens[j].y+aliens[j].height:
+				score+=1
+				aliens.pop(j)
+				j=j-1
+				missiles1.pop(i)
+				i=i-1
+				break
+			j=j+1
+		i=i+1
+	while i<len(missiles2):
+		j=0
+		while j<len(aliens):
+			if aliens[j].x<missiles2[i].x+missiles2[i].width and missiles2[i].x<aliens[j].x+aliens[j].width and aliens[j].y < missiles2[i].y and missiles2[i].y<aliens[j].y+aliens[j].height:
+				aliens[j].death_time=timer+(5*30)
+				aliens[j].color=yellow
+				missiles2.pop(i)
+				i=i-1
+				break
+			j=j+1
+		i=i+1
+	print (score)
 	for alien in aliens:
 		alien.draw()
-	for missile in missiles:
+	for missile in missiles1:
+		missile.y+=missile.speed
+		missile.draw()
+	for missile in missiles2:
 		missile.y+=missile.speed
 		missile.draw()
 	i=0
-	while i < (len(missiles)):
-		if missiles[i].y<-100:
-			missiles.pop(i)
+	while i < (len(missiles1)):
+		if missiles1[i].y<-100:
+			missiles1.pop(i)
 			i=i-1
 		i=i+1
 	pygame.display.update()
