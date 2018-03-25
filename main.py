10a64daaae731b3eba5fed769ee5b80a1a5473de
Swetanjal Dutta <swetanjal.dutta@research.iiit.ac.in>
2018-03-25 15:11:25 +0530

    Final Commit

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ from missile_package import Missile2
 from missile_package import Missile
 from font_package import colors
 
+block_height = 75
+block_width = 100
+fps = 30
+
 def message_to_screen(msg, color=colors.red):
 	"""This function is used to display some text on the screen"""
-	font = pygame.font.SysFont(None, 75)
+	font = pygame.font.SysFont(None, block_height)
 	screen_text = font.render(msg, True, color)
 	gameDisplay.blit(screen_text, [275,650])
 
@@ -30,7 +34,7 @@ def missile_draw(missiles, rate):
 	"""This function is used to update the location of missiles and redraw them on the window"""
 	for missile in missiles:
 		if (timer-missile.birth_time)>0 and (timer-missile.birth_time)%(30/rate) == 0:
-			missile.y=missile.y - 75
+			missile.y=missile.y - block_height
 		missile.draw()
 
 def destroy_aliens(aliens):
@@ -41,6 +45,32 @@ def destroy_aliens(aliens):
 			aliens.pop(i)
 			i=i-1
 		i=i+1
+
+def destroy_missiles(missile_type):
+	"""This function destroys the missiles of missile_type if they go out of the game screen"""
+	i=0
+	while i < (len(missile_type)):
+		if missile_type[i].y<-block_width:
+			missile_type.pop(i)
+			i=i-1
+		i=i+1
+
+def update_graphics():
+	"""This function updates the graphics on the screen by redrawing game objects"""
+	gameDisplay.fill(colors.white)
+	space_ship.draw()
+	message_to_screen("SCORE: "+str(score))
+	for alien in aliens:
+		alien.draw()
+	missile_draw(missiles1, 1)
+	missile_draw(missiles2, 2)
+	pygame.display.update()
+
+def detect_collision(alien, missile):
+	"""This function returns True if alien and missile game objects collide"""
+	if alien.x<missile.x+missile.width and missile.x<alien.x+alien.width and alien.y == missile.y:
+		return True
+	return False
 
 pygame.init()
 gameDisplay = pygame.display.set_mode((800,700))
@@ -59,7 +89,7 @@ missiles1 = []
 missiles2 = []
 
 x , y = spawn_alien(aliens)
-aliens.append(Alien(gameDisplay, x, y, timer+(30*8)))	
+aliens.append(Alien(gameDisplay, x, y, timer+(8*fps)))	
 
 while not gameExit:
 	timer = timer+1
@@ -68,31 +98,29 @@ while not gameExit:
 			if event.key == pygame.K_q:
 				gameExit = True
 			if event.key == pygame.K_d:
-				change=100
-				if space_ship.x<700:
+				change=block_width
+				if space_ship.x<(7*block_width):
 					space_ship.x+=change
 				change=0			
 			if event.key == pygame.K_a:
-				change=-100
+				change=-block_width
 				if space_ship.x>0:
 					space_ship.x+=change
 				change=0			
 		if event.type == pygame.KEYUP:
 			if event.key == pygame.K_SPACE:
-				missiles1.append(Missile1(gameDisplay, space_ship.x, space_ship.y-75, timer))
+				missiles1.append(Missile1(gameDisplay, space_ship.x, space_ship.y-block_height, timer))
 			if event.key == pygame.K_s:
-				missiles2.append(Missile2(gameDisplay, space_ship.x, space_ship.y-75, timer))
-	if timer%300 == 0:
+				missiles2.append(Missile2(gameDisplay, space_ship.x, space_ship.y-block_height, timer))
+	if timer%(10*fps) == 0:
 		x , y = spawn_alien(aliens)
-		aliens.append(Alien(gameDisplay, x, y, timer+(30*8)))
-	destroy_aliens(aliens)
-	gameDisplay.fill(colors.white)
-	space_ship.draw()
+		aliens.append(Alien(gameDisplay, x, y, timer+(8*fps)))
+	
 	i=0
 	while i<len(missiles1):
 		j=0
 		while j<len(aliens):
-			if aliens[j].x<missiles1[i].x+missiles1[i].width and missiles1[i].x<aliens[j].x+aliens[j].width and aliens[j].y == missiles1[i].y:
+			if detect_collision(aliens[j], missiles1[i]):
 				score+=1
 				aliens.pop(j)
 				j=j-1
@@ -105,32 +133,18 @@ while not gameExit:
 	while i<len(missiles2):
 		j=0
 		while j<len(aliens):
-			if aliens[j].x<missiles2[i].x+missiles2[i].width and missiles2[i].x<aliens[j].x+aliens[j].width and aliens[j].y == missiles2[i].y:
-				aliens[j].death_time=timer+(5*30)
+			if detect_collision(aliens[j], missiles2[i]) == True:
+				aliens[j].death_time=timer+(5*fps)
 				aliens[j].color=colors.yellow
 				missiles2.pop(i)
 				i=i-1
 				break
 			j=j+1
 		i=i+1
-	message_to_screen("SCORE: "+str(score))
-	for alien in aliens:
-		alien.draw()
-	missile_draw(missiles1, 1)
-	missile_draw(missiles2, 2)
-	i=0
-	while i < (len(missiles1)):
-		if missiles1[i].y<-100:
-			missiles1.pop(i)
-			i=i-1
-		i=i+1
-	i=0
-	while i < (len(missiles2)):
-		if missiles2[i].y<-100:
-			missiles2.pop(i)
-			i=i-1
-		i=i+1
-	pygame.display.update()
-	clock.tick(30)
+	destroy_aliens(aliens)
+	destroy_missiles(missiles1)
+	destroy_missiles(missiles2)
+	update_graphics()
+	clock.tick(fps)
 pygame.quit()
 quit()
